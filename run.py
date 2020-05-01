fa8ee77369bdb9d21810e47ccea5a8285ce7cd05
EC2 Default User <ec2-user@ip-172-31-45-112.ec2.internal>
2020-05-01 08:23:00 +0000

    Chat Program

--- run.py
+++ run.py
@@ -1,20 +1,41 @@
 import os
-
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
-@app.route('/')
+def add_messages(username, message):
+    """Add messages to the 'messages' list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+    
+def get_all_messages():
+    """Get all messages and seperate them with a 'br'"""
+    return "<br>".join(messages)
+
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        
+    if "username" in session:
+        return redirect(session["username"])
+        
+    return render_template("index.html")
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
     
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect(username)
     
 app.run (host = os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
